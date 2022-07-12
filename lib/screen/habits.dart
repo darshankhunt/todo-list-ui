e79d3497bc524d1ce443417d2439766e9eb2116e
darshankhunt <darshankhunt13@gmail.com>
2022-07-12 18:47:53 +0530

    commit on 12-07-2022

--- lib/screen/habits.dart
+++ lib/screen/habits.dart
@@ -19,12 +19,19 @@ class _HabitsState extends State<Habits> {
             AppBar(
               backgroundColor: const Color(0xffe1efed),
               elevation: 0,
-              leading: const Icon(
-                Icons.arrow_back,
-                color: Color(0xff0e2b25),
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Color(0xff0e2b25),
+                ),
               ),
+              centerTitle: true,
               title: const Text(
                 'Habits',
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Color(0xff0e2b25),
                   fontWeight: FontWeight.w700,
@@ -33,14 +40,49 @@ class _HabitsState extends State<Habits> {
               ),
             ),
             Positioned(
-              top: 10,
-              child: Image.asset('assets/leaf.png'),
+              top: -35,
+              right: -20,
+              child: Image.asset('assets/leaf_rotate_up.png'),
             ),
-            Column(
-              children: [
-                SizedBox(height: 100),
-                Text('April 2022'),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 100),
+                  const Text(
+                    'April 2022',
+                    style: TextStyle(
+                        color: Color(0xff0e2b25),
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '5 task for today',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromARGB(255, 21, 182, 149),
+                        ),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: const Color(0xfff2ffff),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.calendar_month,
+                            color: Color.fromARGB(255, 21, 182, 149),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
